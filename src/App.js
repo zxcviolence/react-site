dc96415f3eabf062e4caf9109e86706b94093eff
Islam <yakubov959@mail.ru>
2023-11-19 16:04:07 +0300

    подключил react-bootstrap и react-router, убрал все лишнее

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Button } from "react-bootstrap";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button variant="primary">Button</Button>
+    </div>
+  );
 }
 
 export default App;
