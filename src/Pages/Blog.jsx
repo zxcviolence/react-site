379efce1d04a073432f3abbe69f722358940d3f5
Islam <yakubov959@mail.ru>
2023-11-21 16:25:10 +0300

    блог посты

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,10 +1,85 @@
 import React from "react";
+import {
+  Card,
+  CardText,
+  Col,
+  Container,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <div className="d-flex m-5">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
+              alt="js"
+              width={150}
+              height={150}
+            />
+            <div className="ms-5">
+              <h5>Blog post</h5>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+                totam nemo beatae repellat libero quasi impedit minima eligendi
+                architecto accusantium expedita explicabo harum eaque incidunt
+                asperiores at nihil perferendis atque illo delectus, veniam
+                officiis est. Iste libero, minus consectetur corporis assumenda
+                autem nobis enim maiores consequatur placeat consequuntur
+                repellat minima atque.
+              </p>
+            </div>
+          </div>
+
+          <div className="d-flex m-5">
+            <img
+              src="https://titrias.com/files/2022/04/typescript.png"
+              alt="js"
+              width={150}
+              height={150}
+            />
+            <div className="ms-5">
+              <h5>Blog post</h5>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
+                totam nemo beatae repellat libero quasi impedit minima eligendi
+                architecto accusantium expedita explicabo harum eaque incidunt
+                asperiores at nihil perferendis atque illo delectus, veniam
+                officiis est. Iste libero, minus consectetur corporis assumenda
+                autem nobis enim maiores consequatur placeat consequuntur
+                repellat minima atque.
+              </p>
+            </div>
+          </div>
+        </Col>
+        <Col md="3">
+          <h5 className="text-center mt-5">Categories</h5>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>HTML/CSS</ListGroup.Item>
+              <ListGroup.Item>Javascript</ListGroup.Item>
+              <ListGroup.Item>Pyton</ListGroup.Item>
+              <ListGroup.Item>Java</ListGroup.Item>
+              <ListGroup.Item>C++</ListGroup.Item>
+            </ListGroup>
+          </Card>
+
+          <Card className="mt-3" bg="light">
+            <Card.Body>
+              <Card.Title>Side widget</Card.Title>
+              <Card.Text>
+                Iste libero, minus consectetur corporis assumenda autem nobis
+                enim maiores consequatur placeat consequuntur repellat minima
+                atque.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
               src="https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
             <Card.Body>
-              <Card.Title>Developers</Card.Title>
+              <Card.Title>Design</Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores earum iusto obcaecati quasi ad fugiat! Corrupti.
@@ -31,7 +31,7 @@ const Home = () => {
               src="https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
             <Card.Body>
-              <Card.Title>SEO</Card.Title>
+              <Card.Title>Developers</Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores earum iusto obcaecati quasi ad fugiat! Corrupti.
@@ -46,7 +46,7 @@ const Home = () => {
               src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             />
             <Card.Body>
-              <Card.Title>Design</Card.Title>
+              <Card.Title>SEO</Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Asperiores earum iusto obcaecati quasi ad fugiat! Corrupti.
