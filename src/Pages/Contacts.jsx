f80ae0c247ddde0536896ce22c39142826299032
Islam <yakubov959@mail.ru>
2023-11-21 13:20:38 +0300

    форма обратной связи

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 const Contacts = () => {
   return (
-    <div>
-      <h1>Contacts</h1>
-    </div>
+    <Container style={{ width: "500px" }}>
+      <h1 className="text-center">Contact Us</h1>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text>We'll never share your email with anyone else</Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows="3" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 };
 
