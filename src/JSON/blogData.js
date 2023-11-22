a5f967c579aa8d35d65395fa3777b4cb5971d2dd
Islam <yakubov959@mail.ru>
2023-11-22 18:54:30 +0300

    ...

---
+++ src/JSON/blogData.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+    title: "Blog post",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur elit. Quae totam nemo beatae repellat libero quasi impedit minima eligendiarchitecto accusantium expedita explicabo harum eaqueincidunt asperiores at nihil perferendis atque illo delectus, veniam officiis est. Iste libero, minus consectetur corporis assumenda autem nobis enim maiores consequatur placeat consequuntur repellat minima atque.",
+  },
+  {
+    id: 2,
+    img: "https://titrias.com/files/2022/04/typescript.png",
+    title: "Blog post",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur elit. Quae totam nemo beatae repellat libero quasi impedit minima eligendiarchitecto accusantium expedita explicabo harum eaqueincidunt asperiores at nihil perferendis atque illo delectus, veniam officiis est. Iste libero, minus consectetur corporis assumenda autem nobis enim maiores consequatur placeat consequuntur repellat minima atque.",
+  },
+];
+
+export const blogCategories = [
+  {
+    id: 1,
+    name: "HTML/CSS",
+  },
+  {
+    id: 2,
+    name: "Javascript",
+  },
+  {
+    id: 3,
+    name: "Typescript",
+  },
+  {
+    id: 4,
+    name: "Python",
+  },
+  {
+    id: 5,
+    name: "Java",
+  },
+  {
+    id: 6,
+    name: "C++",
+  },
+];
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,69 +1,33 @@
 import React from "react";
-import {
-  Card,
-  Col,
-  Container,
-  ListGroup,
-  Row,
-} from "react-bootstrap";
+import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import { data, blogCategories } from "../JSON/blogData";
 
 const Blog = () => {
   return (
     <Container>
       <Row>
         <Col>
-          <div className="d-flex m-5">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
-              alt="js"
-              width={150}
-              height={150}
-            />
-            <div className="ms-5">
-              <h5>Blog post</h5>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-                totam nemo beatae repellat libero quasi impedit minima eligendi
-                architecto accusantium expedita explicabo harum eaque incidunt
-                asperiores at nihil perferendis atque illo delectus, veniam
-                officiis est. Iste libero, minus consectetur corporis assumenda
-                autem nobis enim maiores consequatur placeat consequuntur
-                repellat minima atque.
-              </p>
-            </div>
-          </div>
-
-          <div className="d-flex m-5">
-            <img
-              src="https://titrias.com/files/2022/04/typescript.png"
-              alt="js"
-              width={150}
-              height={150}
-            />
-            <div className="ms-5">
-              <h5>Blog post</h5>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae
-                totam nemo beatae repellat libero quasi impedit minima eligendi
-                architecto accusantium expedita explicabo harum eaque incidunt
-                asperiores at nihil perferendis atque illo delectus, veniam
-                officiis est. Iste libero, minus consectetur corporis assumenda
-                autem nobis enim maiores consequatur placeat consequuntur
-                repellat minima atque.
-              </p>
-            </div>
-          </div>
+          {data.map((item) => {
+            return (
+              <div key={item.id} className="d-flex m-5">
+                <img src={item.img} alt="img" width={150} height={150} />
+                <div className="ms-5">
+                  <h5>{item.title}</h5>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            );
+          })}
         </Col>
         <Col md="3">
           <h5 className="text-center mt-5">Categories</h5>
           <Card>
             <ListGroup variant="flush">
-              <ListGroup.Item>HTML/CSS</ListGroup.Item>
-              <ListGroup.Item>Javascript</ListGroup.Item>
-              <ListGroup.Item>Typescript</ListGroup.Item>
-              <ListGroup.Item>Python</ListGroup.Item>
-              <ListGroup.Item>Java</ListGroup.Item>
-              <ListGroup.Item>C++</ListGroup.Item>
+              {blogCategories.map((item) => {
+                return (
+                  <ListGroup.Item key={item.id}>{item.name}</ListGroup.Item>
+                );
+              })}
             </ListGroup>
           </Card>
 
