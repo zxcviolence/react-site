a53115f5b464f77cc2f6dc182464a441c04f2248
Islam <yakubov959@mail.ru>
2023-11-24 02:00:32 +0300

    подключил api email с помощью emailjs в компоненте 'about', post-запрос через axios, также выскакивает уведомление от sweetalert об успешной отправке

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,25 +1,86 @@
+import axios from "axios";
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const Contacts = () => {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_2ojdj7e";
+    const templateId = "template_0ypvoio";
+    const publicKey = "hJRg1RV0CLkH3jmxs";
+
+    const data = {
+      service_id: serviceId,
+      template_id: templateId,
+      user_id: publicKey,
+      template_params: {
+        from_name: name,
+        from_email: email,
+        to_name: "Dex",
+        message: message,
+      },
+    };
+
+    try {
+      const res = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        data
+      );
+      if (res.data === "OK") {
+        Swal.fire({
+          title: "Good job!",
+          text: "Your email has been sent",
+          icon: "success",
+        });
+      }
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <Container style={{ width: "500px" }}>
       <h1 className="text-center">Contact Us</h1>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            as="input"
+            type="text"
+            placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            as="input"
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Text>We'll never share your email with anyone else</Form.Text>
         </Form.Group>
-
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows="3" />
+          <Form.Control
+            as="textarea"
+            rows="3"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </Form.Group>
-
         <Form.Group controlId="formBasicCheckbox">
-          <Form.Check className="mt-2 mb-2" type="checkbox" label="Check me out" />
-          <Button variant="primary" type="submit">
+          <Button className="mt-2" variant="primary" type="submit">
             Submit
           </Button>
         </Form.Group>
