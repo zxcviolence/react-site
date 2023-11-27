eff7194cf1e9470896dcb4609b648f9727c2c599
Islam <yakubov959@mail.ru>
2023-11-27 15:48:11 +0300

    ...

--- src/JSON/teamData.js
+++ src/JSON/teamData.js
@@ -20,4 +20,11 @@ export const data = [
     img: "https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     buttonText: "About team"
   },
+  {
+    id: 4,
+    title: "Project managment",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores earum iusto obcaecati quasi ad fugiat! Corrupti.",
+    img: "https://plus.unsplash.com/premium_photo-1661255413239-67a0345c4e37?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    buttonText: "About team"
+  },
 ];
